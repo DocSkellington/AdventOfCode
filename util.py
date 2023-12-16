5477c82334e19c643cf7d6cf540b041eaf9d075a
Gaëtan Staquet <gaetan.staquet@gmail.com>
2023-12-16 15:27:52 +0100

    Skip empty input lines

--- util.py
+++ util.py
@@ -31,7 +31,9 @@ def load[T](test: bool, parse: Callable[[str], T]) -> list[T]:
     lines = []
     with open("test" if test else "input", "r", encoding="UTF-8") as inF:
         for line in inF.readlines():
-            lines.append(parse(line.strip()))
+            p = parse(line.strip())
+            if p is not None:
+                lines.append(p)
     return lines
 
 
